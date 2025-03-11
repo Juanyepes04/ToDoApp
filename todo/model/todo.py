15c449f712920dbf092e5e93e300ae507e182aa3
Juan Diego Yepes <jyepes023@soyudemedellin.edu.co>
2025-03-11 13:28:37 -0500

    mark_completed commit

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -5,3 +5,5 @@ class Todo:
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+    def mark_completed(self):
+        self.completed = True
