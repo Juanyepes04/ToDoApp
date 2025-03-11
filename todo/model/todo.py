231001ddab517b00a94c3b72677c3816083b1bc2
Juan Diego Yepes <jyepes023@soyudemedellin.edu.co>
2025-03-11 13:31:39 -0500

    metodo str

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -5,8 +5,14 @@ class Todo:
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+
     def mark_completed(self):
         self.completed = True
+
     def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
+
+    def __str__(self):
+        return f"{self.code_id} - {self.title}"
+
